00bdb485efa7f068962d853252411e70bd6e4251
Aleks <ausau.work@gmail.com>
2026-06-19 09:48:24 +0200

    Add first automated API test script

---
+++ api-automation/test_api.py
@@ -0,0 +1,28 @@
+﻿
+import requests
+
+
+url = "https://jsonplaceholder.typicode.com/users/1"
+
+response = requests.get(url)
+
+data = response.json()
+
+
+
+print("--- STARTING API TEST ---")
+
+if response.status_code == 200:
+    print("✅ Pass: Status code is 200 OK!")
+else:
+    print(f"❌ Fail: Expected 200, but got {response.status_code}")
+
+expected_name = "Bret"
+actual_name = data["username"]
+
+if actual_name == expected_name:
+    print(f"✅ Pass: User name is correctly '{actual_name}'!")
+else:
+    print(f"❌ Fail: Expected '{expected_name}', but got '{actual_name}'")
+
+print("--- TEST COMPLETE ---")
